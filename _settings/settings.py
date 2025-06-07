8cd288b5208cd6a3d4140d66264686ff5c5389cf
SvPr1993 <mrtventana@gmail.com>
2025-06-07 12:30:35 +0300

    lightyearscalc_project

--- _settings/settings.py
+++ _settings/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = '_settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
             ],
         },
     },
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+if DEBUG:
+    STATICFILES_DIRS = (BASE_DIR / 'static',)
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- app_calc/views.py
+++ app_calc/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 import json
+from django.template import loader
 
 
 def input_data(request):
+    template = loader.get_template("index.html")
     planet_name = input("Введите название планеты: ")
     destination_sum = input("Введите количество световых лет до планеты: ")
     full_data = json.dumps({"1": planet_name, "2": destination_sum})
