6500b2cd4dd4a29096b930b65174d0e5feb75678
SvPr1993 <mrtventana@gmail.com>
2025-10-07 19:15:24 +0300

    lightyearcalc_project

--- app_planets/service.py
+++ app_planets/service.py
@@ -1,6 +1,15 @@
-from app_planets.repo import planet_info_repo
+from app_planets.repo import planet_info_repo, destination_sum_repo
 
 
 def planets_info_service(planet_name: str):
     distance = planet_info_repo(planet_name)
+    print("Ответ репозитория", distance)
     return distance
+
+
+def destination_sum_service(destination_sum: int):
+    destination_const_km = destination_sum_repo()
+    print(type(destination_const_km), type(destination_sum))
+    full_sum_km = destination_sum * destination_const_km
+    print("full_sum_km", full_sum_km)
+    return full_sum_km
