130055f950ebd240a99c31c5dce8d045156ea7c3
SvPr1993 <mrtventana@gmail.com>
2025-10-14 19:15:40 +0300

    lightyearcalc_project

--- app_planets/urls.py
+++ app_planets/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+
+from app_planets import views
 from app_planets.views import planet_info_views, destination_sum_views
 
 urlpatterns = [
     path("", planet_info_views, name="planets_info"),
     path("distance/", destination_sum_views, name="destination_sum"),
+    path("distance/<str:planet>", destination_sum_views, name="destination_sum"),
+    path("error/", views.error, name="error"),
      ]
--- app_planets/views.py
+++ app_planets/views.py
@@ -12,21 +12,25 @@ def planet_info_views(request):
             answer = f"До планеты {planet_name} расстояние равно {planets_distance} километров"
             return render(request, "app_planets/planets_info.html", {"result": answer})
         else:
-            return redirect("destination_sum")
+            return redirect("destination_sum", planet_name)
 
     else:
         return render(request, "app_planets/planets_info.html")
 
 
-def destination_sum_views(request):
+def destination_sum_views(request, planet=""):
     print("Сработала вторая вьев,,,,,,,,,,,,,,,,,,,,,")
-    #Данный код срабатывает если планеты не в базе джаго админ
+    print(planet)
+    # Данный код срабатывает если планеты не в базе джанго админ
     if request.method == 'POST':
         destination_sum = request.POST.get("destination_sum")
-        int_destination_sum = int(destination_sum)
-        new_planets_distance = destination_sum_service(int_destination_sum)
-        print(new_planets_distance, "++++++++++++++++")
-        answer = f"До планеты {'Планета'} расстояние равно {new_planets_distance} километров"
+        try:
+            int_destination_sum = int(destination_sum)
+            new_planets_distance = destination_sum_service(int_destination_sum)
+            answer = f"До планеты {planet} расстояние равно {new_planets_distance} километров"
+        except:
+            message_error = "Вы ввели не число!"
+            answer = message_error
         return render(request, "app_planets/destination_sum.html", {"result": answer})
 
     else:
@@ -34,8 +38,15 @@ def destination_sum_views(request):
 
 
 def error(request):
-    return render(request, "error.html")
+    return render(request, "app_planets/error.html")
 
-# Сделать так чтобы в отевте фигурировала планета из 1 функции
-# Обработать случай если вместо цифр вводятся другие типы данных во второй функции
+# Добавить во второй шаблон поле для планеты, если случай если напрямую там нет введенной планеты, если есть то планета планета там тоже есть
 # Показать проект фронт энд проекта space adventure
+
+
+# ТЗ к сайту:
+# Сайт на главной странице должен иметь внешний вид свайперов https://russianspacesystems.ru/
+# на сайте должно быть 2 формы отправки в базу данных на определенное количество записей с именами в двух случаях
+# на сайте должна быть форма напоминания о событии на почту или мобильный телефон
+# на сайте должна быть форма отправки обратной связи с неограниченным вводом текста
+# на сайте должен быть поиск записей по сайту
